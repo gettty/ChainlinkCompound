e487efa619ae58d28b0bbc2076ee457d08f3fd5b
gettyhelm <32806801+gettyhelm@users.noreply.github.com>
2021-07-17 23:58:31 -0600

    update

--- reserves.js
+++ reserves.js
@@ -17,26 +17,7 @@ let packet = {};
 let reserveData = {};
 
 let multiple18 = new BigNumber (10).pow(18);
-let multiple30 = new BigNumber (10).pow(30);
-let multiple28 = new BigNumber (10).pow(28);
 let multiple8 = new BigNumber (10).pow(8);
-let multiple6 = new BigNumber (10).pow(6);
-
-let decimals = {
-    '0x0000000000000000000000000000000000000000': multiple18,
-    '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48': multiple6,
-    '0x6B175474E89094C44Da98b954EedeAC495271d0F': multiple18,
-    '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599': multiple28,
-    '0xdAC17F958D2ee523a2206206994597C13D831ec7': multiple6,
-    '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984': multiple18,
-    '0xc00e94Cb662C3520282E6f5717214004A7f26888': multiple18,
-    '0xE41d2489571d322189246DaFA5ebDe1F4699F498': multiple18,
-    '0x0D8775F648430679A709E98d2b0Cb6250d2887EF': multiple18,
-    '0x0000000000085d4780B73119b644AE5ecd22b376': multiple18,
-    '0x514910771AF9Ca656af840dff83E8264EcF986CA': multiple18,
-    '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359': multiple18,
-    '0x1985365e9f78359a9B6AD760e32412f4a445E862': multiple18,
-}
 
 async function getReserveData (){
     let text = "";
@@ -59,7 +40,7 @@ async function getReserveData (){
         chainlinkInfo[i] = {'cToken': config['cToken'],'underlying': config['underlying'],'price':price}
         //begin 
         const ctokenabi = JSON.parse(fs.readFileSync("cethABI.json"));
-        console.log(config['cToken']);
+        //console.log(config['cToken']);
         const ctokenContract = new ethers.Contract(config['cToken'],ctokenabi,provider);
         const erc20 = new ethers.Contract(chainlinkInfo[i]["underlying"],erc20abi,provider);
         let multiple = 0;
